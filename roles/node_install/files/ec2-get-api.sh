71ff424c0ee6fe31cc3a30fbdd78e0132a4c0280
ashokks80 <58323161+ashokks80@users.noreply.github.com>
2023-01-30 12:57:26 +0530

    Add files via upload

---
+++ roles/node_install/files/ec2-get-api.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+apigateway=${aws apigatewayv2 get-apis --region us-east-1|grep -i https|grep -E -o 'https://\w+.execute-api.us-east-1.amazonaws.com'}
+export $apigateway
