931554548b6dceb7e577e0625c39b9a261094609
ashokks80 <58323161+ashokks80@users.noreply.github.com>
2023-01-30 12:59:56 +0530

    Update ec2-get-api.sh

--- roles/node_install/files/ec2-get-api.sh
+++ roles/node_install/files/ec2-get-api.sh
@@ -2,5 +2,6 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-apigateway=${aws apigatewayv2 get-apis --region us-east-1|grep -i https|grep -E -o 'https://\w+.execute-api.us-east-1.amazonaws.com'}
-export $apigateway
+APIGATEWAY=${aws apigatewayv2 get-apis --region us-east-1|grep -i https|grep -E -o 'https://\w+.execute-api.us-east-1.amazonaws.com'}
+export $APIGATEWAY
+echo "$APIGATEWAY URL THIS IS"
