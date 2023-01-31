ff3e3dfb9e32605b9bc40040510650fdb3a9410c
ashokks80 <58323161+ashokks80@users.noreply.github.com>
2023-01-31 09:57:13 +0530

    Update vpcpeer.tf

--- vpcpeer.tf
+++ vpcpeer.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "vpc-peering" {
   peer_owner_id = "646752868508"
-  peer_vpc_id   = var.peerid.default
+  peer_vpc_id   = "vpc-0d4e6f0468b8b227a"
   vpc_id        = aws_vpc.crud-task.id
   peer_region   = "us-east-1"
   auto_accept = "true"
