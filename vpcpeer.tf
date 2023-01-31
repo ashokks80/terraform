d125abc830f7a16d740bb5217894514b7521b951
ashokks80 <58323161+ashokks80@users.noreply.github.com>
2023-01-31 09:48:54 +0530

    Update vpcpeer.tf

--- vpcpeer.tf
+++ vpcpeer.tf
@@ -1,22 +1,6 @@
-# provider "aws" {
-#   region     = "us-east-1"
-# }
-
-module "single_account_single_region" {
-  source  = "grem11n/vpc-peering/aws//examples/single-account-single-region"
-
-  providers = {
-    aws.this = aws
-    aws.peer = aws
-  }
-
-  this_vpc_id = aws_vpc.crud-task.id
-  peer_vpc_id = var.peerid.default
-
-  auto_accept_peering = true
-
-  tags = {
-    Name        = "tf-single-account-single-region"
-    Environment = "Test"
-  }
+resource "aws_vpc_peering_connection" "vpc-peering" {
+  peer_owner_id = "646752868508"
+  peer_vpc_id   = var.peerid.default
+  vpc_id        = aws_vpc.crud-task.id
+  peer_region   = "us-east-1"
 }
