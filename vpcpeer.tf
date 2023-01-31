86fc7938cd8fc056fec61291f8e62ad9d763cdeb
ashokks80 <58323161+ashokks80@users.noreply.github.com>
2023-01-31 09:08:40 +0530

    Create vpcpeer.tf

---
+++ vpcpeer.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+module "single_account_single_region" {
+  source = "../../"
+
+  providers = {
+    aws.this = aws
+    aws.peer = aws
+  }
+
+  this_vpc_id = aws_vpc.crud-task.id
+  peer_vpc_id = var.peerid.id
+
+  auto_accept_peering = true
+
+  tags = {
+    Name        = "tf-single-account-single-region"
+    Environment = "Test"
+  }
+}
