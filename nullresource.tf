2abfbfc00f74d05154a9d6023bd7e556405c3f86
ashokks80 <58323161+ashokks80@users.noreply.github.com>
2023-02-11 20:35:52 +0530

    Update nullresource.tf

--- nullresource.tf
+++ nullresource.tf
@@ -4,7 +4,7 @@ resource "null_resource" "node-server" {
        sleep 180;
        >inv1.ini;
          echo "[node-server]" | tee -a inv1.ini;     
-         echo "${aws_instance.node-server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${var.key_name}.pem ansible_python_interpreter=/usr/bin/python3"  | tee -a inv1.ini;
+         echo "${aws_instance.node-server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../${var.key_name}.pem ansible_python_interpreter=/usr/bin/python3"  | tee -a inv1.ini;
          export ANSIBLE_HOST_KEY_CHECKING=False;
        ansible-playbook -i inv1.ini main.yml -vvv;
       EOT
